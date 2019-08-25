8b590708b402e8a3ef8d3b3c8469fd2233506646
Harold Martin <harold.martin@gmail.com>
2019-08-25 06:53:40 -0700

    CSS (dark mode) injection and time formatting

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 function getTriggerNavButton(dagId) {
   const li = document.createElement('li');
   const a = document.createElement('a');
@@ -11,7 +12,7 @@ function getTriggerNavButton(dagId) {
   return li;
 }
 
-function airflow() {
+const airflow = () => {
   if (document.title.indexOf('Airflow') === -1) { return; }
   if (window.location.pathname === '/admin/') {
     const alerts = document.getElementsByClassName('alert-info');
@@ -45,7 +46,38 @@ function airflow() {
     const ul = document.getElementsByClassName('nav-pills')[0];
     const dagId = window.location.search.match(/dag_id=(\w*)/)[1];
     ul.appendChild(getTriggerNavButton(dagId));
+
+    const states = document.getElementsByClassName('state');
+    for (const el of states) {
+      if (el.dataset && el.dataset.originalTitle) {
+        const duration = el.dataset.originalTitle.match(/Duration: (\d+(\.\d+)?)/);
+        if (duration) {
+          const durationSeconds = parseFloat(duration[1]);
+          let durationFormatted = 'Duration: ';
+          if (durationSeconds > 3600) {
+            durationFormatted += `${(durationSeconds / 3600).toFixed(2)} h`;
+          } else if (durationSeconds > 60) {
+            durationFormatted += `${(durationSeconds / 60).toFixed(2)} m`;
+          } else {
+            durationFormatted += durationSeconds;
+          }
+          const newTitle = el.dataset.originalTitle.replace(duration[0], durationFormatted);
+          el.dataset.originalTitle = newTitle;
+        }
+      }
+    }
   }
+};
+
+if (document.title.indexOf('Airflow') !== -1) {
+  // inject dark mode css
+  const link = document.createElement('link');
+  // eslint-disable-next-line no-undef
+  link.href = chrome.runtime.getURL('src/inject/dark.css');
+  link.type = 'text/css';
+  link.rel = 'stylesheet';
+  document.getElementsByTagName('head')[0].appendChild(link);
 }
 
+// document.addEventListener('DOMContentLoaded', airflow, false);
 airflow();
